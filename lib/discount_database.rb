8a32e5037deb18d79dfd7d2122373adf03cb7368
steven-klavins <stevenklavins94@gmail.com>
2025-10-22 10:39:07 +0100

    update DiscountDatabase to support one_per_customer discount types

--- lib/discount_database.rb
+++ lib/discount_database.rb
@@ -2,15 +2,19 @@ class DiscountDatabase
   def initialize
     # Mimic discount DB
     @discounts = {
-      apple: :buy_one_get_one_free,
-      pear: :buy_one_get_one_free,
-      banana: :half_price,
-      pineapple:  :half_price,
-      mango: :buy_3_get_1_free
+      apple: {type: :buy_one_get_one_free, one_per_customer: false},
+      pear: {type: :buy_one_get_one_free, one_per_customer: false},
+      banana: {type: :half_price, one_per_customer: false},
+      pineapple: {type: :half_price, one_per_customer: true},
+      mango: {type: :buy_3_get_1_free, one_per_customer: false}
     }
   end
 
   def type(item)
-    @discounts[item]
+    @discounts[item]&.[](:type)
+  end
+
+  def one_per_customer?(item)
+    @discounts[item]&.[](:one_per_customer)
   end
 end
